f307acdcd0a92e696a867a1bd22bb6f89e9d6b20
Dave J <davejanovec@gmail.com>
2019-04-22 09:18:38 -0500

    logo

--- OnTrac/src/app/component/header/header.component.ts
+++ OnTrac/src/app/component/header/header.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  title: string = "OnTrac"
+  
 
+  
   constructor() { }
-
+  
   ngOnInit() {
   }
 
